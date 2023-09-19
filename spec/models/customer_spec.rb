28facee22b56acf4ad552ebcd8952721585fa243
K.D. Hubbard <hubbard.kd@gmail.com>
2023-09-19 15:50:50 -0600

    add shouldamatchers gem

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do 
+    it { should have_many :subscriptions }
+  end
+
+  describe "validations" do 
+
+  end
 end
