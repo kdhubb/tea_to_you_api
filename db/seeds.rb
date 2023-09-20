e15d3681b5ecefb47ee886e4e4097946a412fedf
K.D. Hubbard <hubbard.kd@gmail.com>
2023-09-20 10:43:05 -0600

    wip seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+TeaSubscription.destroy_all
+Subscription.destroy_all
+Tea.destroy_all
+Customer.destroy_all
+
+customers = create_list(:customer, 25)
+
+customers[0].subscriptions.create(:subscription)
--- spec/factories/add_customer_to_subscriptions.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :add_customer_to_subscription do
-    
-  end
-end
